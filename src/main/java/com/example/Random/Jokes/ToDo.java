477fe769d7ae8d5900709b8259777312c4c7a3f1
Hrishi456-cmd <rkhandbhor45@gmail.com>
2023-05-19 22:49:43 +0530

    Submitted

---
+++ src/main/java/com/example/Random/Jokes/ToDo.java
@@ -0,0 +1,13 @@
+package com.example.Random.Jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDo.class, args);
+	}
+
+}
